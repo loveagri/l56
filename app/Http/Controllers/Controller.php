af5e9257586cf3b33c83e67a47d540c4c072e93a
loveagri <984070691@qq.com>
2019-04-17 14:34:03 +0800

    diff

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,22 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Common\Auto\JwtAuth;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use App\Http\Response\ResponseJson;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ResponseJson;
-
-
-
-    public function index(){
-        return $this->jsonSuccessData(['hello'=>'123']);
-    }
-
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,90 +2,72 @@
 
 namespace App\Http\Controllers;
 
-use App\Common\Auto\JwtAuth;
-use App\Common\Err\ApiErrDesc;
-use App\Exceptions\ApiException;
-use App\Http\Response\ResponseJson;
+use Illuminate\Http\Request;
 use App\User;
-use Illuminate\Support\Facades\Redis;
 
-class UserController extends UserBaseController
+class UserController extends Controller
 {
-
-    use ResponseJson;
-
-    public function info()
+    /*
+     * 个人介绍页面
+     */
+    public function show(User $user)
     {
-        $user = User::where('id', $this->uid)->first();
 
-        if (!$user) {
-            throw new ApiException(ApiErrDesc::ERR_USER_NOT_EXIST);
-        }
+        // 这个人的文章
+        $posts = $user->posts()->orderBy('created_at', 'desc')->take(10)->get();
+        // 这个人的关注／粉丝／文章
+        $user = \App\User::withCount(['stars', 'fans', 'posts'])->find($user->id);
+        $fans = $user->fans()->get();
+        $stars = $user->stars()->get();
 
-        return $this->jsonSuccessData(
-            [
-                'name' => $user->name,
-                'email' => $user->email,
-            ]
-        );
+        return view("user/show", compact('user', 'posts', 'fans', 'stars'));
     }
 
+    public function fan(User $user)
+    {
+        $me = \Auth::user();
+        \App\Fan::firstOrCreate(['fan_id' => $me->id, 'star_id' => $user->id]);
+        return [
+            'error' => 0,
+            'msg' => ''
+        ];
+    }
 
+    public function unfan(User $user)
+    {
+        $me = \Auth::user();
+        \App\Fan::where('fan_id', $me->id)->where('star_id', $user->id)->delete();
+        return [
+            'error' => 0,
+            'msg' => ''
+        ];
+    }
 
+    public function setting()
+    {
+        $me = \Auth::user();
+        return view('user/setting', compact('me'));
+    }
 
+    public function settingStore(Request $request, User $user)
+    {
+        $this->validate(request(),[
+            'name' => 'min:3',
+        ]);
 
+        $name = request('name');
+        if ($name != $user->name) {
+            if(\App\User::where('name', $name)->count() > 0) {
+                return back()->withErrors(array('message' => '用户名称已经被注册'));
+            }
+            $user->name = request('name');
+        }
+        if ($request->file('avatar')) {
+            $path = $request->file('avatar')->storePublicly(md5(\Auth::id() . time()));
+            $user->avatar = "/storage/". $path;
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-//     public function infoWithCache()
-//     {
-// //        $cacheUserInfo = Redis::get('uid:'.$this->uid);
-// //        if (!$cacheUserInfo) {
-//             $user = User::where('id', $this->uid)->first();
-//             if (!$user) {
-//                 throw new ApiException(ApiErrDesc::ERR_USER_NOT_EXIST);
-//             }
-
-//             Redis::setex('uid:'.$this->uid, 3600, json_encode($user->toArray()));
-// //        } else {
-// //            $user = json_decode($cacheUserInfo);
-// //        }
-
-//         return $this->jsonSuccessData(
-//             [
-//                 'name' => $user->name,
-//                 'email' => $user->email,
-//             ]
-//         );
-//     }
+        $user->save();
+        return back();
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
